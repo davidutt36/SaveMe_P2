3425e5d1e4e13b211ec7ca35b980fd2613de7992
David Utt <davidutt36@gmail.com>
2017-01-23 17:09:02 -0500

    Second Commit
    File Setup to integrate Gulp Scaffold and Sass

---
+++ www/scripts/main.js
@@ -0,0 +1,12 @@
+var grid = clickableGrid(10,10,function(el,row,col,i){
+  console.log("You clicked on subject:", el);
+  console.log("You clicked on row:", row);
+  console.log("You clicked on subject:", col);
+  console.log("You clicked on Number:", i);
+
+
+});
+
+function clickableGrid(rows,cols, callback){
+
+};
